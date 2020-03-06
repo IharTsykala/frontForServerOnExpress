5993c75414655c6fbe230d0be419563b87e07c1d
Ihar Tsykala <Sudja09@bk.ru>
2020-03-06 23:54:38 +0300

    ref: refactor code

--- src/components/PaginationBlock/PaginationBlock.tsx
+++ src/components/PaginationBlock/PaginationBlock.tsx
@@ -1,67 +1,84 @@
 import React, { useEffect, useState } from "react"
 import PaginationBlockCSS from "./PaginationBlock.module.css"
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import InputLabel from '@material-ui/core/InputLabel';
+import Select from "@material-ui/core/Select"
+import MenuItem from "@material-ui/core/MenuItem"
+import InputLabel from "@material-ui/core/InputLabel"
 import { connect } from "react-redux"
 import { User } from "../../Redux/interfaces/user.interface"
 import Service from "../../services/service-user"
-
 
 type PaginationBlockProps = {
   user: User
   dispatch: any
   checked: boolean
-  valueSearchBox: String | ''
+  valueSearchBox: String | ""
   setUsers: any
 }
 
-const PaginationBlock:  React.FunctionComponent<PaginationBlockProps> = ({
+const PaginationBlock: React.FunctionComponent<PaginationBlockProps> = ({
   user,
   dispatch,
   checked,
   valueSearchBox,
   setUsers
 }) => {
-    const[numberPage, setNumberPage] =  useState(1)
-    const[limitRender, setLimitRender] = useState(0)
-    
+  const [numberPage, setNumberPage] = useState(1)
+  const [limitRender, setLimitRender] = useState(0)
 
-    // useEffect(() => {
-     
-    // }, [input])
+  // useEffect(() => {
 
-      const getUserAfterPaginationAndSearchAndFilter = async (numberPage:number, limitRender:number) => {
-        let body = {}
-        if(valueSearchBox.length>2) {
-          body = Object.assign({}, body, {idLogInUser: user._id, valueSearchBox, numberPage, checked, limitRender})         
-        } else body = Object.assign({}, body, {idLogInUser: user._id, valueSearchBox:'', numberPage, checked, limitRender})
-        const listUsers = await Service.getUserAfterPaginationAndSearchAndFilter(body)
-        setUsers(listUsers)
-      }
+  // }, [input])
 
-      const handleChangeSelect = (limitRender:any) => {        
-        setLimitRender(+limitRender)
-        getUserAfterPaginationAndSearchAndFilter(numberPage, +limitRender)
-        }
+  const getUserAfterPaginationAndSearchAndFilter = async (
+    numberPage: number,
+    limitRender: number
+  ) => {
+    let body = {}
+    if (valueSearchBox.length > 2) {
+      body = Object.assign({}, body, {
+        idLogInUser: user._id,
+        valueSearchBox,
+        numberPage,
+        checked,
+        limitRender
+      })
+    } else
+      body = Object.assign({}, body, {
+        idLogInUser: user._id,
+        valueSearchBox: "",
+        numberPage,
+        checked,
+        limitRender
+      })
+    const listUsers = await Service.getUserAfterPaginationAndSearchAndFilter(
+      body
+    )
+    setUsers(listUsers)
+  }
 
-      const handlerClickPrevPage = () => {
-        if(numberPage) {
-          setNumberPage(numberPage-1)
-          getUserAfterPaginationAndSearchAndFilter(numberPage-1, limitRender)
-        } 
-      }
+  const handleChangeSelect = (limitRender: any) => {
+    setLimitRender(+limitRender)
+    getUserAfterPaginationAndSearchAndFilter(numberPage, +limitRender)
+  }
 
-      const handlerClickNextPage = () => {
-        setNumberPage(numberPage+1)
-        getUserAfterPaginationAndSearchAndFilter(numberPage+1, limitRender)
-      }
+  const handlerClickPrevPage = () => {
+    if (numberPage) {
+      setNumberPage(numberPage - 1)
+      getUserAfterPaginationAndSearchAndFilter(numberPage - 1, limitRender)
+    }
+  }
 
-    return (
-        <section className={PaginationBlockCSS.All_Users__Pagination_Block}>
-            <div className={PaginationBlockCSS.All_Users__Pagination_Block__Select_block}>        
-        
-            <InputLabel htmlFor="outlined-age-native-simple">
+  const handlerClickNextPage = () => {
+    setNumberPage(numberPage + 1)
+    getUserAfterPaginationAndSearchAndFilter(numberPage + 1, limitRender)
+  }
+
+  return (
+    <section className={PaginationBlockCSS.All_Users__Pagination_Block}>
+      <div
+        className={PaginationBlockCSS.All_Users__Pagination_Block__Select_block}
+      >
+        <InputLabel htmlFor="outlined-age-native-simple">
           Amount Users
         </InputLabel>
         <Select
@@ -70,24 +87,26 @@ const PaginationBlock:  React.FunctionComponent<PaginationBlockProps> = ({
           onChange={e => handleChangeSelect(e.target.value)}
           labelWidth={40}
           inputProps={{
-            name: 'age',
-            id: 'outlined-age-native-simple',
+            name: "age",
+            id: "outlined-age-native-simple"
           }}
-        >  
-          <option value={''}></option>
-          <option value={0}>{'all users'}</option>
+        >
+          <option value={""}></option>
+          <option value={0}>{"all users"}</option>
           <option value={2}>2</option>
           <option value={4}>4</option>
           <option value={8}>8</option>
         </Select>
-            </div>            
-            <div className={PaginationBlockCSS.All_Users__Pagination_Block__Button_block}>
-            <button onClick={()=>handlerClickPrevPage()} >prev</button>
-            <div>{numberPage}</div>
-            <button onClick={()=>handlerClickNextPage()}>next</button>   
-            </div>                        
-        </section>
-    )
+      </div>
+      <div
+        className={PaginationBlockCSS.All_Users__Pagination_Block__Button_block}
+      >
+        <button onClick={() => handlerClickPrevPage()}>prev</button>
+        <div>{numberPage}</div>
+        <button onClick={() => handlerClickNextPage()}>next</button>
+      </div>
+    </section>
+  )
 }
 
 const mapStateToProps = (state: any) => ({
@@ -95,4 +114,3 @@ const mapStateToProps = (state: any) => ({
 })
 
 export default connect(mapStateToProps)(PaginationBlock)
-
