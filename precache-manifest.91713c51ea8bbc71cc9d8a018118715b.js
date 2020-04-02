0a39b06d7a15694d417828d735f7a0821023bee7
Ihar Tsykala <Sudja09@bk.ru>
2020-04-02 14:18:39 +0300

    Updates

---
+++ precache-manifest.91713c51ea8bbc71cc9d8a018118715b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d620b57a6e745b86d935149ab199f03a",
+    "url": "/frontForServerOnExpress/index.html"
+  },
+  {
+    "revision": "b7e739ab7dc3bc6bfa0e",
+    "url": "/frontForServerOnExpress/static/css/2.c2d4b67f.chunk.css"
+  },
+  {
+    "revision": "3f42f1037dff051790ba",
+    "url": "/frontForServerOnExpress/static/css/main.2a462cd1.chunk.css"
+  },
+  {
+    "revision": "b7e739ab7dc3bc6bfa0e",
+    "url": "/frontForServerOnExpress/static/js/2.162433b0.chunk.js"
+  },
+  {
+    "revision": "32da2c8235d3abfdac0ab18d498a59e0",
+    "url": "/frontForServerOnExpress/static/js/2.162433b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f42f1037dff051790ba",
+    "url": "/frontForServerOnExpress/static/js/main.12cb84ec.chunk.js"
+  },
+  {
+    "revision": "fcf9d348406aff11aa16",
+    "url": "/frontForServerOnExpress/static/js/runtime-main.d2201fcc.js"
+  }
+]);
