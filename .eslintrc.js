7031e8e1f9a8f5a3b2a0bab84b1925e4d2cee5bc
Ihar Tsykala <Sudja09@bk.ru>
2020-04-06 01:48:07 +0300

    fix: fix eslint\prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb', 'react-app', 'prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -19,10 +16,27 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
+    'jsx-quotes': [1, 'prefer-double'],
+    'semi-style': ['warn', 'last'],
+    'no-plusplus': 'off',
+    'eol-last': 'warn',
+    'max-len': ['warn', 80, { ignoreUrls: true }],
+    'no-tabs': 'off',
+    indent: ['warn', 2],
+    'no-console': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'import/no-named-as-default': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    "react/jsx-filename-extension": ['warn', { "extensions": [".js", ".jsx",".tsx"] }],
+    'react/jsx-props-no-spreading': 'off',
   },
-};
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,56 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import createSagaMiddleware from 'redux-saga';
-import AllUsersPage from './pages/allUsersPage/allUsersPage';
-import UserInformationPage from './pages/userInformationPage/userInformationPage';
-import GetLoginPage from './pages/GetLoginPage/GetLoginPage';
-import GetLogUpPage from './pages/GetLogUpPage/GetLogUpPage';
-import { GetStartPage } from './pages/GetStartPage/GetStartPage';
-import Navbar from './components/Navbar/Navbar';
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import createSagaMiddleware from 'redux-saga'
+import AllUsersPage from './pages/allUsersPage/allUsersPage'
+import UserInformationPage from './pages/userInformationPage/userInformationPage'
+import GetLoginPage from './pages/GetLoginPage/GetLoginPage'
+import GetLogUpPage from './pages/GetLogUpPage/GetLogUpPage'
+import { GetStartPage } from './pages/GetStartPage/GetStartPage'
+import Navbar from './components/Navbar/Navbar'
 import {
   PrivateRoute,
   defaultPrivateRouteProps,
-} from './PrivateRoutes/PrivateRouteForUsers';
+} from './PrivateRoutes/PrivateRouteForUsers'
 import {
   PrivateRouteForAdmins,
   defaultPrivateRouteForAdminsProps,
-} from './PrivateRoutes/PrivateRouteForAdmins';
-import GetAlbumByID from './pages/GetAlbumByID/GetAlbumByID';
-import PageFriends from './pages/pageFriends/pageFriends';
-import AllAlbumsPage from './pages/allAlbumsPage/allAlbumsPage';
-import AllPhotosPage from './pages/allPhotosPage/allPhotosPage';
-import NotFound from './pages/NotFoundPage/NotFound';
-import { AdminAllUsers } from './pages/AdminPages/AdminAllUser/AdminAllUser';
-import { UserEditInformation } from './pages/UserEditInformationPage/UserEditInformationPage';
-import rootReducer from './Redux/store';
-import DialogsPage from './pages/DialogsPage/DialogsPage';
-import getUserOwnerThisPageForSaga from './Redux/store/userOwnerThisPage/userOwnerThisPage.sagas';
-import putInStoreNewMessageforCurrentDialogSaga from './Redux/store/listMessagesForCurrentDialog/listMessagesForCurrentDialog.sagas';
-import putInStoreAllUsersSaga from './Redux/store/allUsers/allUsers.sagas';
-import userLoginSaga from './Redux/store/userLogin/userLogin.sagas';
-import avatarSaga from './Redux/store/avatar/avatar.sagas';
-import dialogSaga from './Redux/store/dialogs/dialogs.sagas';
-import albumsSaga from './Redux/store/albums/albums.sagas';
+} from './PrivateRoutes/PrivateRouteForAdmins'
+import GetAlbumByID from './pages/GetAlbumByID/GetAlbumByID'
+import PageFriends from './pages/pageFriends/pageFriends'
+import AllAlbumsPage from './pages/allAlbumsPage/allAlbumsPage'
+import AllPhotosPage from './pages/allPhotosPage/allPhotosPage'
+import NotFound from './pages/NotFoundPage/NotFound'
+import { AdminAllUsers } from './pages/AdminPages/AdminAllUser/AdminAllUser'
+import { UserEditInformation } from './pages/UserEditInformationPage/UserEditInformationPage'
+import rootReducer from './Redux/store'
+import DialogsPage from './pages/DialogsPage/DialogsPage'
+import getUserOwnerThisPageForSaga from './Redux/store/userOwnerThisPage/userOwnerThisPage.sagas'
+import putInStoreNewMessageforCurrentDialogSaga from './Redux/store/listMessagesForCurrentDialog/listMessagesForCurrentDialog.sagas'
+import putInStoreAllUsersSaga from './Redux/store/allUsers/allUsers.sagas'
+import userLoginSaga from './Redux/store/userLogin/userLogin.sagas'
+import avatarSaga from './Redux/store/avatar/avatar.sagas'
+import dialogSaga from './Redux/store/dialogs/dialogs.sagas'
+import albumsSaga from './Redux/store/albums/albums.sagas'
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMiddleware = createSagaMiddleware()
 
 const store = createStore(
   rootReducer,
   composeWithDevTools({ trace: true, traceLimit: 25 })(
-    applyMiddleware(sagaMiddleware),
-  ),
-);
+    applyMiddleware(sagaMiddleware)
+  )
+)
 
-sagaMiddleware.run(getUserOwnerThisPageForSaga);
-sagaMiddleware.run(putInStoreNewMessageforCurrentDialogSaga);
-sagaMiddleware.run(putInStoreAllUsersSaga);
-sagaMiddleware.run(userLoginSaga);
-sagaMiddleware.run(avatarSaga);
-sagaMiddleware.run(dialogSaga);
-sagaMiddleware.run(albumsSaga);
+sagaMiddleware.run(getUserOwnerThisPageForSaga)
+sagaMiddleware.run(putInStoreNewMessageforCurrentDialogSaga)
+sagaMiddleware.run(putInStoreAllUsersSaga)
+sagaMiddleware.run(userLoginSaga)
+sagaMiddleware.run(avatarSaga)
+sagaMiddleware.run(dialogSaga)
+sagaMiddleware.run(albumsSaga)
 
 export const App: React.FC = () => (
   <Provider store={store}>
@@ -128,4 +128,4 @@ export const App: React.FC = () => (
       </div>
     </BrowserRouter>
   </Provider>
-);
+)
