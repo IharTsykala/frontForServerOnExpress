57432eee88e0c6f75d75f25f396b9f617c1ec220
Ihar Tsykala <Sudja09@bk.ru>
2020-04-24 21:53:00 +0300

    fix: fix navbar logic render

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,8 @@ import NotFound from "./pages/NotFoundPage/NotFound"
 import { AdminAllUsers } from "./pages/AdminPages/AdminAllUser/AdminAllUser"
 import { UserEditInformation } from "./pages/UserEditInformationPage/UserEditInformationPage"
 import rootReducer from "./Redux"
+import ScopedCssBaseline from "@material-ui/core/ScopedCssBaseline"
+import Box from "@material-ui/core/Box"
 import DialogsPage from "./pages/DialogsPage/DialogsPage"
 import avatarSaga from "./Redux/store/avatar/avatar.sagas"
 import dialogSaga from "./Redux/store/dialogs/dialogs.sagas"
@@ -49,81 +51,83 @@ sagaMiddleware.run(userSaga)
 export const App: React.FC = () => (
   <Provider store={store}>
     <BrowserRouter>
-      <div className="wrapper">
-        <Navbar />
-        <main className="main">
-          <Switch>
-            <Route
-              component={GetStartPage}
-              path="/frontForServerOnExpress"
-              exact
-            />
-            <Route component={GetLoginPage} path="/LogIn" exact />
-            <Route component={GetLogUpPage} path="/SignUp" exact />
-            <PrivateRoute
-              {...defaultPrivateRouteProps}
-              component={DialogsPage}
-              path="/:id/dialogs"
-              exact
-            />
-            <PrivateRoute
-              {...defaultPrivateRouteProps}
-              component={AllUsersPage}
-              path="/user/allUsers"
-              exact
-            />
-            <PrivateRoute
-              {...defaultPrivateRouteProps}
-              component={UserInformationPage}
-              path="/user/profile/:id"
-              exact
-            />
-            <PrivateRoute
-              {...defaultPrivateRouteProps}
-              component={PageFriends}
-              path="/user/:id/friends"
-              exact
-            />
-            <PrivateRoute
-              {...defaultPrivateRouteProps}
-              component={GetAlbumByID}
-              path="/user/:id/album"
-              exact
-            />
-            <PrivateRoute
-              {...defaultPrivateRouteProps}
-              component={AllAlbumsPage}
-              path="/user/:id/allAlbums"
-              exact
-            />
-            <PrivateRoute
-              {...defaultPrivateRouteProps}
-              component={AllPhotosPage}
-              path="/user/:id/allPhotos"
-              exact
-            />
-            <PrivateRoute
-              {...defaultPrivateRouteProps}
-              component={UserEditInformation}
-              path="/user/:id/edit"
-              exact
-            />
-            <PrivateRouteForAdmins
-              {...defaultPrivateRouteForAdminsProps}
-              component={AdminAllUsers}
-              path="/admin/all"
-              exact
-            />
-            <PrivateRouteForAdmins
-              {...defaultPrivateRouteForAdminsProps}
-              component={AdminAllUsers}
-              path="/admin/:id"
-              exact
-            />
-            <Route path="*" component={NotFound} />
-          </Switch>
-        </main>
-      </div>
+      <ScopedCssBaseline>
+        <Box component="div" className="wrapper">
+          <Navbar />
+          <Box component="main" className="main">
+            <Switch>
+              <Route
+                component={GetStartPage}
+                path="/social-network-client"
+                exact
+              />
+              <Route component={GetLoginPage} path="/LogIn" exact />
+              <Route component={GetLogUpPage} path="/SignUp" exact />
+              <PrivateRoute
+                {...defaultPrivateRouteProps}
+                component={DialogsPage}
+                path="/:id/dialogs"
+                exact
+              />
+              <PrivateRoute
+                {...defaultPrivateRouteProps}
+                component={AllUsersPage}
+                path="/user/allUsers"
+                exact
+              />
+              <PrivateRoute
+                {...defaultPrivateRouteProps}
+                component={UserInformationPage}
+                path="/user/profile/:id"
+                exact
+              />
+              <PrivateRoute
+                {...defaultPrivateRouteProps}
+                component={PageFriends}
+                path="/user/:id/friends"
+                exact
+              />
+              <PrivateRoute
+                {...defaultPrivateRouteProps}
+                component={GetAlbumByID}
+                path="/user/:id/album"
+                exact
+              />
+              <PrivateRoute
+                {...defaultPrivateRouteProps}
+                component={AllAlbumsPage}
+                path="/user/:id/allAlbums"
+                exact
+              />
+              <PrivateRoute
+                {...defaultPrivateRouteProps}
+                component={AllPhotosPage}
+                path="/user/:id/allPhotos"
+                exact
+              />
+              <PrivateRoute
+                {...defaultPrivateRouteProps}
+                component={UserEditInformation}
+                path="/user/:id/edit"
+                exact
+              />
+              <PrivateRouteForAdmins
+                {...defaultPrivateRouteForAdminsProps}
+                component={AdminAllUsers}
+                path="/admin/all"
+                exact
+              />
+              <PrivateRouteForAdmins
+                {...defaultPrivateRouteForAdminsProps}
+                component={AdminAllUsers}
+                path="/admin/:id"
+                exact
+              />
+              <Route path="*" component={NotFound} />
+            </Switch>
+          </Box>
+        </Box>
+      </ScopedCssBaseline>
     </BrowserRouter>
   </Provider>
 )
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,17 +5,25 @@ import { connect } from "react-redux"
 import { User } from "../../Redux/entitiesInterface/user.interface"
 import { getUserRefresh } from "../../Redux/store/user/user.actions"
 import { logOutUserForAllDevices } from "../../Redux/store/user/user.actions"
+import {
+  AppBar,
+  Toolbar,
+  ListItem,
+  Typography,
+  Avatar,
+  Box,
+} from "@material-ui/core"
 
 type NavbarProps = {
-  user: User
-  dispatch: any
-  loadingState: String
+  user: User,
+  dispatch: any,
+  loadingState: String,
 }
 
 const Navbar: React.FunctionComponent<NavbarProps> = ({
   user,
   dispatch,
-  loadingState
+  loadingState,
 }) => {
   const getUserAfterLogInAndRefresh = useCallback(() => {
     if (localStorage.getItem("token")) dispatch(getUserRefresh())
@@ -28,80 +36,82 @@ const Navbar: React.FunctionComponent<NavbarProps> = ({
   const handlerLogOut = () => dispatch(logOutUserForAllDevices(user._id, user))
 
   return (
-    <>
-      <nav className={NavbarCSS.navbar_container}>
-        {(loadingState === "loading" && <p>Ожидайте ответа</p>) ||
-          (loadingState === "notFound" && <p>not found</p>) ||
-          (loadingState === "loaded" && (
-            <>
-              {user.login && (
-                <>
-                  <a
-                    href="/"
-                    className={`${NavbarCSS.navbar__avatar__brand_logo} brand-logo`}
-                  >
-                    {`Hello, ${user.login}`}
-                  </a>
-                  {user.avatar && (
-                    <img
-                      className={NavbarCSS.navbar__avatar}
-                      src={`http://localhost:8080/images/users/${user._id}/${user.avatar}`}
-                      alt="avatar"
-                    />
-                  )}
-                  {!user.avatar && (
-                    <img
-                      className={NavbarCSS.navbar__avatar}
-                      src={`http://localhost:8080/images/pattern-avatar.jpg`}
-                      alt="avatar"
-                    />
-                  )}
-                </>
-              )}
-              {!user.login && (
-                <a href="/" id={"incognito"} className="brand-logo">
-                  {`Hello, Incognito`}
-                </a>
-              )}
-              <ul className="right hide-on-med-and-down">
-                <li>
+    <AppBar position="static" className={NavbarCSS.navbar_container}>
+      {(loadingState === "loading" && (
+        <Typography variant="h6">Ожидайте ответа</Typography>
+      )) ||
+        (loadingState === "notFound" && (
+          <Typography variant="h6">not found</Typography>
+        )) ||
+        (loadingState === "loaded" && (
+          <>
+            <Box component="div" className="navbar__user">
+              <Typography
+                variant="h6"
+                className={`${NavbarCSS.navbar__avatar__brand_logo} brand-logo`}
+              >
+                {(user.login && `Hello, ${user.login}`) || "Hello, incognito"}
+              </Typography>
+              <Avatar
+                className={NavbarCSS.navbar__avatar}
+                src={
+                  (user.avatar &&
+                    `http://localhost:8080/images/users/${user._id}/${user.avatar}`) ||
+                  "http://localhost:8080/images/pattern-avatar.jpg"
+                }
+                alt="avatar"
+              />
+            </Box>
+            <Toolbar component="ul" className="right hide-on-med-and-down">
+              <ListItem component="li" button={(!user._id && false) || true}>
+                {user._id && (
                   <NavLink to={`/user/profile/${user._id}`}>
-                    UserProfile
+                    <Typography variant="h6">UserProfile</Typography>
                   </NavLink>
-                </li>
-                <li>
-                  <NavLink to={`/user/allUsers`}>All Users</NavLink>
-                </li>
-                <li>
-                  <NavLink to={`/${user._id}/dialogs`}>My Dialogs</NavLink>
-                </li>
-                {user.login && (
-                  <li onClick={() => handlerLogOut()}>
-                    <NavLink to="/frontForServerOnExpress">Log Out</NavLink>
-                  </li>
                 )}
-                {!user.login && (
-                  <>
-                    <li>
-                      <NavLink to="/LogIn">Log In</NavLink>
-                    </li>
-                    <li>
-                      <NavLink to="/SignUp">Sign Up</NavLink>
-                    </li>
-                  </>
+              </ListItem>
+              <ListItem button={(!user._id && false) || true}>
+                {user._id && (
+                  <NavLink to={`/user/allUsers`}>
+                    <Typography variant="h6">All Users</Typography>
+                  </NavLink>
                 )}
-              </ul>
-            </>
-          )) ||
-          (loadingState === "error" && <h1>ошибка</h1>)}
-      </nav>
-    </>
+              </ListItem>
+              <ListItem button>
+                <NavLink to={(user._id && `/${user._id}/dialogs`) || "/LogIn"}>
+                  <Typography variant="h6">
+                    {(user._id && "My Dialogs") || "Log In"}
+                  </Typography>
+                </NavLink>
+              </ListItem>
+
+              <ListItem
+                button
+                onClick={() => {
+                  user._id && handlerLogOut()
+                }}
+              >
+                <NavLink
+                  to={(user._id && "/social-network-client") || "/SignUp"}
+                >
+                  <Typography variant="h6">
+                    {(user._id && "Log Out") || "Sign Up"}
+                  </Typography>
+                </NavLink>
+              </ListItem>
+            </Toolbar>
+          </>
+        )) ||
+        (loadingState === "error" && (
+          <Typography variant="h6">ошибка</Typography>
+        ))}
+    </AppBar>
   )
 }
 
 const mapStateToProps = (state: any) => ({
   user: state.user.user,
-  loadingState: state.loadingState.loadingState
+  loadingState: state.loadingState.loadingState,
 })
 
 export default connect(mapStateToProps)(Navbar)
