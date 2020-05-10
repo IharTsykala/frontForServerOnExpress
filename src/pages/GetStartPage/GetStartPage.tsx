9583ca02ba9ceb579be9bc384d992006291ff5c4
Ihar Tsykala <Sudja09@bk.ru>
2020-05-10 23:09:37 +0300

    feat: add styles on start page

--- src/pages/GetStartPage/GetStartPage.tsx
+++ src/pages/GetStartPage/GetStartPage.tsx
@@ -1,12 +1,20 @@
 import React from "react"
 import GetStartPageCSS from "./GetStartPage.module.css"
+import { Typography, Box } from "@material-ui/core"
 
 export const GetStartPage: React.FC = () => {
   return (
-    <>
-      <h1
+    <Box
+      component={"div"}
+      className={GetStartPageCSS.main__start_page__container}
+    >
+      <Typography
+        variant="h2"
+        component="h2"
         className={GetStartPageCSS.main__container__start_page__header}
-      >{`You are in the best application :))`}</h1>
-    </>
+      >
+        {`You are in the best application :))`}
+      </Typography>
+    </Box>
   )
 }
